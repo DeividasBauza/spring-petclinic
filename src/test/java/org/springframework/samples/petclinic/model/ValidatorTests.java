c7f3df3160e974a387c0d44855b39d21363fad6c
Deividas Bauza <afrolietuvis.da@gmail.com>
2021-01-19 09:21:52 +0200

    changed test for integration testing purposes

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -44,7 +44,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("notEmptyy");
+		person.setFirstName("notEmptyyy");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
